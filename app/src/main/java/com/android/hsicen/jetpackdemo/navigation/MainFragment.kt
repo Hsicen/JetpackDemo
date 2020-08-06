34b9c6f153e741aa708d3dd1e263bbaaf9b2cddc
hsicen <codinghuang@163.com>
2020-08-06 09:34:10 +0800

    首次提交 Navigation相关用例实践

---
+++ app/src/main/java/com/android/hsicen/jetpackdemo/navigation/MainFragment.kt
@@ -0,0 +1,86 @@
+package com.android.hsicen.jetpackdemo.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import com.android.hsicen.jetpackdemo.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        //方法1
+        btn_to_second.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString("name", "Mike")
+            bundle.putBoolean("on", true)
+            bundle.putInt("age", 23)
+
+            val navOptions = NavOptions.Builder()
+                .setEnterAnim(R.anim.fragment_fade_enter)
+                .setExitAnim(R.anim.fragment_fade_exit)
+                .setPopEnterAnim(R.anim.nav_default_pop_enter_anim)
+                .setPopExitAnim(R.anim.nav_default_pop_exit_anim)
+                .build()
+
+            Navigation.findNavController(it)
+                .navigate(R.id.action_mainFragment_to_secondFragment, bundle, navOptions)
+        }
+
+        //方法2
+        //btn_to_second.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_mainFragment_to_secondFragment))
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
